336b8bd4a2ee75433ff929e236557dd1a09b8239
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-03-30 18:11:36 +0530

    refactor: update SignDialog button styles for improved visibility and user experience

--- frontend/app/landing/Navbar/Signdialog.tsx
+++ frontend/app/landing/Navbar/Signdialog.tsx
@@ -122,7 +122,7 @@ const SignDialog: React.FC = () => {
         <button
           type="button"
           onClick={openModal}
-          className="rounded-md bg-Blueviolet px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          className="rounded-md border border-Blueviolet px-4 py-2 text-sm font-medium text-Blueviolet hover:bg-opacity-10 hover:bg-Blueviolet focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
           Sign In
         </button>
