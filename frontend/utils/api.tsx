34db246f3d3ebc471cbc8645f93657f6838295ee
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-03-31 16:17:51 +0530

    refactor: update API_BASE_URL to use production endpoint

--- frontend/utils/api.tsx
+++ frontend/utils/api.tsx
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const API_BASE_URL = "https://qcode.api.altero.dev";
 
 export const API_ROUTES = {
   LOGIN: `${API_BASE_URL}/user/login`,
